530babad6ac1ed0a50ee23d749f0e29425b73fb1
Carrol2709 <111042693+Carrol2709@users.noreply.github.com>
2022-10-17 10:19:55 +0200

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * parser - Receives the main string and all the necessary parameters to
- * print a formated string.
+ * parser - Receives the main string and all other parameters to print.
  * @format: A string containing all the desired characters.
  * @f_list: A list of all the posible functions.
  * @arg_list: A list containing all the argumentents passed to the program.
